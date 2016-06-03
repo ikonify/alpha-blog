f5a6b6d9d54166854246592cd0aee52dc39777be
Rails Udemy Course <hemisalo@gmail.com>
2016-06-03 08:40:06 +0000

    Created form partial, added destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,14 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    flash[:notice] = "Article was succesfully deleted"
+    redirect_to articles_path
+  end
+  
   private
     def article_params
       # This whitelists the parameters
