b9759a76535bacfdce96ffc7d93d6d4bdd53f9db
Rails Udemy Course <hemisalo@gmail.com>
2016-06-09 10:23:47 +0000

    Add shot action and add gravatar images to users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80})
+    # Gravatar images are stored as MD5 hashes of the user emails
+    # gravatar_id gets the MD5 hash of users email
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
